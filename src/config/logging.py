4494ba73551b6329f4776c6c592279c9e008a3fd
andyalv <andy.red02@gmail.com>
2026-03-16 23:29:21 -0600

    Add show_path parameter to start_logger for configurable logging path display

--- src/config/logging.py
+++ src/config/logging.py
@@ -2,12 +2,14 @@ import logging
 from rich.logging import RichHandler
 
 
-def start_logger(level: int = logging.INFO, datefmt: str = "[%X]") -> logging.Logger:
+def start_logger(
+    level: int = logging.INFO, datefmt: str = "[%X]", show_path: bool = True
+) -> logging.Logger:
     logging.basicConfig(
         level=level,
         format="%(message)s",
         datefmt=datefmt,
-        handlers=[RichHandler(omit_repeated_times=False, show_path=True)],
+        handlers=[RichHandler(omit_repeated_times=False, show_path=show_path)],
     )
 
     return logging.getLogger("rich")
