3f54a2e4c6dc0662100121046b571f22d2e35e04
andyalv <andy.red02@gmail.com>
2026-03-17 04:11:54 -0600

    Refactor logging configuration to improve clarity and maintainability

--- src/config/logging.py
+++ src/config/logging.py
@@ -2,14 +2,22 @@ import logging
 from rich.logging import RichHandler
 
 
-def start_logger(
-    level: int = logging.INFO, datefmt: str = "[%X]", show_path: bool = True
-) -> logging.Logger:
+def configure_logging(
+    level: int = logging.DEBUG, datefmt: str = "[%X]", show_path: bool = True
+) -> None:
     logging.basicConfig(
         level=level,
         format="%(message)s",
         datefmt=datefmt,
         handlers=[RichHandler(omit_repeated_times=False, show_path=show_path)],
+        force=True,
     )
 
+
+# Start logger with name "rich" and return the logger instance.
+# The logger is configured with the specified logging level, date format, and whether to show the file path in log messages.
+def start_logger(
+    level: int = logging.DEBUG, datefmt: str = "[%X]", show_path: bool = True
+) -> logging.Logger:
+    configure_logging(level=level, datefmt=datefmt, show_path=show_path)
     return logging.getLogger("rich")
